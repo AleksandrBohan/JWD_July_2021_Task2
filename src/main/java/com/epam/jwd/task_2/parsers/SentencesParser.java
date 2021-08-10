6fa007eac6c4fc7839c7732299f6de7b922b5dd6
Alex_Boh <aleksandrbohan2@gmail.com>
2021-08-10 23:04:04 +0300

    I did first steps in project creation.

--- src/main/java/com/epam/jwd/task_2/parsers/SentencesParser.java
+++ src/main/java/com/epam/jwd/task_2/parsers/SentencesParser.java
@@ -1,4 +1,16 @@
 package com.epam.jwd.task_2.parsers;
 
 public class SentencesParser {
+    private int numberOfWords;
+    private WordsParser wordsParser;
+    String sentenseParser = "^[" + new WordsParser().getWordParser() + "]+$";
+
+
+    public String getSentenseParser() {
+        return sentenseParser;
+    }
+
+    public void setSentenseParser(String sentenseParser) {
+        this.sentenseParser = sentenseParser;
+    }
 }
--- src/main/java/com/epam/jwd/task_2/parsers/WordsParser.java
+++ src/main/java/com/epam/jwd/task_2/parsers/WordsParser.java
@@ -1,4 +1,28 @@
 package com.epam.jwd.task_2.parsers;
 
 public class WordsParser {
+
+    private int numberOfSymbols;
+
+    String wordParser;
+
+    public WordsParser(){
+        setWordParser("^\\s+\\w\\s+$");
+    }
+
+    public int getNumberOfSymbols() {
+        return numberOfSymbols;
+    }
+
+    public void setNumberOfSymbols(int numberOfSymbols) {
+        this.numberOfSymbols = numberOfSymbols;
+    }
+
+    public String getWordParser() {
+        return wordParser;
+    }
+
+    public void setWordParser(String wordParser) {
+        this.wordParser = wordParser;
+    }
 }
