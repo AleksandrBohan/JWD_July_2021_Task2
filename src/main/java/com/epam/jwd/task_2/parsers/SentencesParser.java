2f5ced32f55598d2dcd14ea705b54de41e61c7e8
Alex_Boh <aleksandrbohan2@gmail.com>
2021-08-14 01:06:15 +0300

    I added SentencesParser and WordsParser classes in Text class.

--- src/main/java/com/epam/jwd/task_2/parsers/SentencesParser.java
+++ src/main/java/com/epam/jwd/task_2/parsers/SentencesParser.java
@@ -1,5 +1,7 @@
 package com.epam.jwd.task_2.parsers;
 
+import com.epam.jwd.task_2.exceptions.WrongFileName;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,10 +9,12 @@ public class SentencesParser {
 
     private final static String sentenceParce = "([^(\\\\.|!|\\\\?)]+)(\\.|!|\\?)";
 
-    public void parseIt(String text, String parser) {
+    public void parseIt(String text, String parser, String path) throws WrongFileName {
         Pattern pattern = Pattern.compile(parser);
 
         Matcher matcher = pattern.matcher(text);
+
+        new FileParser().checkFilePath(path);
 
         while (matcher.find()) {
             System.out.println("Found: " + matcher.group(0) +
@@ -25,10 +29,11 @@ public class SentencesParser {
     }
 
     public static void main(String[] args) {
-        SentencesParser sentencesParser = new SentencesParser();
+       /* SentencesParser sentencesParser = new SentencesParser();
         sentencesParser.parseIt("Ajhffj kfl fkk f fkkf kf. " +
                 "ddjjd kjfdk kd kdkd kd  kdk lldk   kdk!" +
-                "jfkfk kfkk kfkkfd lldl?", sentencesParser.getSentenceParce());
+                "jfkfk kfkk kfkkfd lldl?", sentencesParser.getSentenceParce());*/
+        //TODO
     }
 
 }
--- src/main/java/com/epam/jwd/task_2/parsers/TextParser.java
+++ src/main/java/com/epam/jwd/task_2/parsers/TextParser.java
@@ -1,5 +1,20 @@
 package com.epam.jwd.task_2.parsers;
 
+import com.epam.jwd.task_2.exceptions.WrongFileName;
+
+import java.util.Scanner;
+
 public class TextParser {
+
+    private WordsParser wordsParser;
+
+    private SentencesParser sentencesParser;
+
+    public TextParser(String text, String path) throws WrongFileName {
+
+        sentencesParser.parseIt(text, sentencesParser.getSentenceParce(), path);
+        wordsParser.parseIt(text, wordsParser.getWordParser(), path);
+
+    }
 
 }
